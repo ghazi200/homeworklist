c2546cd52ba93ebe23d4b868140a83b5a237df91
ghazi200 <techworldstarzllc@gmail.com>
2019-05-08 13:51:06 -0400

    Add files via upload

---
+++ ingredients.py
@@ -0,0 +1,30 @@
+"""" INGREDIENTS.PY"""
+
+
+import sys
+
+ingredients = [
+    "Butter"
+    
+    "Milk"
+    
+    "Salt"
+    
+    "Eggs"
+    
+    "Oil"
+    
+    "Flour"
+    
+    "Bananas"
+]
+
+n= len(ingredients)
+
+p= 0
+while p < n:
+    print()
+    print(ingredients [p])
+    n += 1
+
+sys.exit(0)
